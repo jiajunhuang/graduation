eeeced93edf4c99fbcc48aeef398be5b85dfd01c
jiajunhuang <gansteed@gmail.com>
2016-02-23 15:25:14 +0800

    add image

--- models/goods.py
+++ models/goods.py
@@ -11,6 +11,7 @@ class Goods(ORMBase):
     __tablename__ = "goods"
 
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    image = Column(String(1024), nullable=False, default="")  # 商品图片路径
     name = Column(String(64), nullable=False)  # 商品名称
     seller = Column(Integer, nullable=False, index=True)  # 售卖者
     create_at = Column(DateTime, nullable=False, default=datetime.datetime.now())  # 上架时间
--- models/users.py
+++ models/users.py
@@ -11,6 +11,7 @@ class Users(ORMBase):
     __tablename__ = "users"
 
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    avatar = Column(String(1024), nullable=False, default="")  # 头像路径
     level = Column(Integer, nullable=False, default=0, index=True)  # 用户级别，0为普通用户，1为商家，2为管理员
     phone = Column(String(11), nullable=False, index=True)  # 注册手机号码
     name = Column(String(24), nullable=False, index=True)  # 用户名
