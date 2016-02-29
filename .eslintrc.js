dc7d257c6f0303cb5cdb2f4e089907d6d49de4c7
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-03-01 00:44:36 +0800

    tab = two spaces and add ignoring rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,26 +5,26 @@ module.exports = {
     'html'
   ],
   'env': {
-      'browser': true,
-      'es6': true,
-      'node': true
+    'browser': true,
+    'es6': true,
+    'node': true
   },
   'plugins': [
     'html'
   ],
   'ecmaFeatures': {
-      'arrowFunctions': true,
-      'destructuring': true,
-      'classes': true,
-      'defaultParams': true,
-      'blockBindings': true,
-      'modules': true,
-      'objectLiteralComputedProperties': true,
-      'objectLiteralShorthandMethods': true,
-      'objectLiteralShorthandProperties': true,
-      'restParams': true,
-      'spread': true,
-      'templateStrings': true
+    'arrowFunctions': true,
+    'destructuring': true,
+    'classes': true,
+    'defaultParams': true,
+    'blockBindings': true,
+    'modules': true,
+    'objectLiteralComputedProperties': true,
+    'objectLiteralShorthandMethods': true,
+    'objectLiteralShorthandProperties': true,
+    'restParams': true,
+    'spread': true,
+    'templateStrings': true
   },
   // add your custom rules here
   'rules': {
@@ -38,30 +38,30 @@ module.exports = {
     'block-scoped-var': 1,
     'default-case': 1,
     'dot-notation': [2, {
-        'allowKeywords': false,
-        'allowPattern': '^[a-z]+(_[a-z]+)+$'
+      'allowKeywords': false,
+      'allowPattern': '^[a-z]+(_[a-z]+)+$'
     }],
     'eqeqeq': [2, 'smart'],
     'no-extend-native': 1,
     'no-implicit-coercion': 0,
     'no-native-reassign': 1,
     'no-redeclare': [2, {
-        'builtinGlobals': true
+      'builtinGlobals': true
     }],
     'no-warning-comments': [1, {
-        'terms': ['TODO', 'HACK', 'XXX', 'FIXME'],
-        'location': 'start'
+      'terms': ['TODO', 'HACK', 'XXX', 'FIXME'],
+      'location': 'start'
     }],
     'vars-on-top': 0,
     'wrap-iife': [2, 'any'],
     'init-declarations': 0,
     'no-unused-expressions': [2, {
-        'allowShortCircuit': true,
-        'allowTernary': true
+      'allowShortCircuit': true,
+      'allowTernary': true
     }],
     'no-unused-vars': [2, {
-        'vars': 'local',
-        'args': 'after-used'
+      'vars': 'local',
+      'args': 'after-used'
     }],
     'brace-style': 2,
     'func-style': 0,
@@ -69,34 +69,34 @@ module.exports = {
     'consistent-this': [2, 'that'],
     'linebreak-style': [2, 'unix'],
     'key-spacing': [2, {
-        'beforeColon': false,
-        'afterColon': true
+      'beforeColon': false,
+      'afterColon': true
     }],
-    'indent': [2, 4, {
-        'SwitchCase': 2
+    'indent': [2, 2, {
+      'SwitchCase': 2
     }],
     'lines-around-comment': [2, {
-        'beforeBlockComment': true,
-        'afterBlockComment': true
+      'beforeBlockComment': true,
+      'afterBlockComment': true
     }],
     'new-parens': 2,
     'no-inline-comments': 2,
     'no-lonely-if': 2,
     'no-mixed-spaces-and-tabs': 2,
     'no-multiple-empty-lines': [2, {
-        'max': 3,
-        'maxEOF': 1
+      'max': 3,
+      'maxEOF': 1
     }],
     'no-negated-condition': 2,
     'no-nested-ternary': 1,
     'no-trailing-spaces': [2, {
-        'skipBlankLines': true
+      'skipBlankLines': true
     }],
     'no-ternary': 0,
     'no-restricted-syntax': [2, 'WithStatement'],
     'object-curly-spacing': [2, 'always', {
-        'objectsInObjects': false,
-        'arraysInObjects': false
+      'objectsInObjects': false,
+      'arraysInObjects': false
     }],
     'one-var': [2, 'never'],
     'operator-assignment': [2, 'never'],
@@ -108,14 +108,14 @@ module.exports = {
     'keyword-spacing': 2,
     'space-before-blocks': 2,
     'space-before-function-paren': [2, {
-        'anonymous': 'always',
-        'named': 'never'
+      'anonymous': 'always',
+      'named': 'never'
     }],
     'max-len': [2, 120, 4],
     'max-params': [1, 4],
     'max-statements': [2, 50],
     'no-plusplus': [2,{
-        'allowForLoopAfterthoughts': true
+      'allowForLoopAfterthoughts': true
     }],
     'comma-dangle': [2, 'never'],
     'no-sequences': 2,
