8ef6ef48f3a5e2d3fafb4cddb6cb901371be89ac
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-03-26 20:33:50 +0800

    header and fix-navbar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import IndexView from './components/IndexView.vue'
-// import ShopView from './components/ShopView.vue'
-// import UserView from './components/UserView.vue'
+import IndexView from './components/IndexView.vue'
+import ShopView from './components/ShopView.vue'
+import UserView from './components/UserView.vue'
 import App from './App'
 
 Vue.use(Router)
@@ -10,28 +10,24 @@ Vue.use(Router)
 // routing
 var router = new Router()
 
-// router.map({
-//   '/index/': {
-//     component: IndexView
-//   },
-//   '/user/': {
-//     component: UserView
-//   },
-//   '/shop/:id': {
-//     component: ShopView
-//   }
-// })
+router.map({
+  '/index': {
+    component: IndexView
+  },
+  '/user/': {
+    component: UserView
+  },
+  '/shop/:id': {
+    component: ShopView
+  }
+})
 
-// router.beforeEach(function () {
-//   window.scrollTo(0, 0)
-// })
+router.redirect({
+  '*': '/index'
+})
 
-// router.redirect({
-//   '*': 'index'
-// })
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
 
 router.start(App, '#app')
-// new Vue({
-//   el: '#app',
-//   components: { App }
-// })
