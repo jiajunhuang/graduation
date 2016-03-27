373e30208239690877bd8a2c3c7cf667a39d67cc
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-03-27 19:53:21 +0800

    add Sliders

---
+++ src/components/Sliders.vue
@@ -0,0 +1,21 @@
+<template >
+<div class="sliders">
+  <img src="../assets/sliders/yo_1.png" alt="慧眼识银" />
+</div>
+</template>
+
+<style lang="sass" scoped>
+.sliders {
+  width: 1000px;
+  margin: 20px auto;
+  img {
+    width: 100%;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'Sliders'
+}
+</script>
