782207d19bfb4d644776d708aa6305234a20bcd8
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-03-27 20:50:05 +0800

    import FixedNavbar

---
+++ src/components/ShopView.vue
@@ -0,0 +1,17 @@
+<template >
+<fixed-navbar></fixed-navbar>
+</template>
+
+<style lang="sass">
+</style>
+
+<script>
+import FixedNavbar from './FixedNavbar.vue'
+
+
+export default {
+  name: 'ShopView',
+  components: [ FixedNavbar ]
+}
+
+</script>
