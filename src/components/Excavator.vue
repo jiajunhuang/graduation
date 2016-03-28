3c4b11ffd6f12deaf73d8d53b4bd39041d38a1b1
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-03-28 21:18:56 +0800

    add

---
+++ src/components/Excavator.vue
@@ -0,0 +1,117 @@
+<template >
+  <div id="excavator">
+    <div class="filter">
+      <span class="category">商家分类：</span>
+      <div class="contents clearfix">
+        <a href="" class="focus">全部商家</a>
+        <a href="" class="hot">品牌商家</a>
+        <a href="">快餐类</a>
+        <a href="">正餐类</a>
+        <a href="">小零食</a>
+      </div>
+    </div>
+    <div class="control clearfix">
+      <div class="sort clearfix">
+        <a href="#" class="focus">默认排序</a>
+        <a href="#" >销量好</a>
+        <a href="#">评价好</a>
+        <a href="#">
+          <span>其他排序</span>
+          <div class="others">
+
+          </div>
+        </a>
+        <a href="#">起送价格</a>
+      </div>
+      <div class="option clearfix">
+        <label ><input type="checkbox"> 新开商家</label>
+        <label ><input type="checkbox"> 免费配送</label>
+        <label ><input type="checkbox"> 在线支付</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+  #excavator {
+    width: 1000px;
+    margin: 0 auto;
+    background-color: #fff;
+    border: 1px solid #e6e6e6;
+  }
+  div.filter {
+    padding: 10px 10px;
+
+    span.category {
+      float: left;
+      color: #999;
+      padding: 0 10px;
+      margin-right: 8px;
+      line-height: 34px;
+    }
+    .contents {
+      a {
+        float: left;
+        padding: 0px 10px;
+        border-radius: 3px;
+        white-space: nowrap;
+        display: inline-block;
+        margin: 5px 6px;
+        text-decoration: none;
+        color: #666;
+        &:hover {
+          background-color: #f6f6f6;
+        }
+        &.focus {
+          color: #FFF;
+          background-color: #0089dc;
+        }
+        &.hot {
+          color: #f74342;
+        }
+      }
+    }
+  }
+  div.control {
+    background-color: #f5f5f5;
+    line-height: 40px;
+
+    div.sort {
+      float: left;
+      margin-left: 10px;
+      a {
+        text-decoration: none;
+        color: #666;
+        padding: 0 7px;
+        float: left;
+        &.focus {
+          color: #0089dc;
+        }
+      }
+    }
+    div.option {
+      float: right;
+      margin-right: 15px;
+      position: relative;
+      label {
+        margin-left: 10px;
+      }
+      &:before {
+        content: ' ';
+        display: inline-block;
+        position: absolute;
+        width: 1px;
+        height: 30px;
+        background-color: #e6e6e6;
+        top: 6px;
+        left: -15px;
+      }
+    }
+  }
+</style>
+
+<script>
+  export default {
+    name: 'Excavator'
+  }
+</script>
