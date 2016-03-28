3fe55c768b5347b368fa97b14a075c9819e7c034
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-03-28 21:19:19 +0800

    move

---
+++ src/views/IndexView.vue
@@ -0,0 +1,20 @@
+<template>
+  <sliders></sliders>
+  <excavator></excavator>
+  <fixed-navbar></fixed-navbar>
+</template>
+
+<script>
+import Sliders from '../components/Sliders'
+import FixedNavbar from '../components/FixedNavbar'
+import Excavator from '../components/Excavator'
+
+export default {
+  name: 'IndexView',
+  components: [ Sliders, FixedNavbar, Excavator ]
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/views/ShopView.vue
@@ -0,0 +1,18 @@
+<template >
+  <h2>shop</h2>
+  <fixed-navbar></fixed-navbar>
+</template>
+
+<style lang="sass" scoped>
+</style>
+
+<script>
+import FixedNavbar from '../components/FixedNavbar'
+
+
+export default {
+  name: 'ShopView',
+  components: [ FixedNavbar ]
+}
+
+</script>
