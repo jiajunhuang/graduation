26d2ca91795f316968407d37140df6c5c387d0a8
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-04-03 15:21:25 +0800

    stop.....

--- test/unit/Header.spec.js
+++ test/unit/Header.spec.js
@@ -1,20 +1,40 @@
 /* global describe, it, expect */
 
-
 import Vue from 'vue'
 import Header from '../../src/components/Header.vue'
 
 describe('Header.vue', function () {
-  // asserting JavaScript options
-  it('show have correct name', function () {
+
+  it('should have correct name', function () {
     expect(Header.name).toBe('TopHeader')
   })
 
   it('should have correct default data', function () {
     expect(Header.data().isShowDropDown).toBe(false)
+    expect(Header.data().focusItem).toBe(0)
   })
 
-  // it('')
+
+  var vm = new Vue({
+    template: '<div><test></test></div>',
+    components: {
+      test: Header
+    }
+  }).$mount()
+
+  it('should be Vue instance', function () {
+    expect(vm instanceof Vue).toBe(true)
+  })
+
+  it('should have correct switchItem function', function () {
+    expect(typeof Header.methods.toggleDropDown).toBe('function')
+    expect(vm.isShowDropDown).toBe(false)
+    // Header.methods.toggleDropDown()
+    // console.log(Header.data().isShowDropDown)
+    // expect(Header.data().isShowDropDown).toBe(true)
+    // Header.methods.toggleDropDown()
+    // expect(Header.data().isShowDropDown).toBe(false)
+  })
 
 
   // asserting rendered result by actually rendering the component
