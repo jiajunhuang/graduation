eeb405595b466b81125f477c54d1b5c842141a24
jiajunhuang <gansteed@gmail.com>
2016-04-07 10:13:14 +0800

    Update phone length.

--- models/user.py
+++ models/user.py
@@ -14,7 +14,7 @@ class User(ORMBase):
     avatar = Column(String(1024), nullable=False, default="")  # 头像路径
     level = Column(Integer, nullable=False, default=0, index=True)  # 用户级别，0为普通用户，1为商家，2为管理员
     passwd = Column(String(64), nullable=False)  # 密码
-    phone = Column(String(11), nullable=False)  # 注册手机号码
+    phone = Column(String(24), nullable=False)  # 注册手机号码
     name = Column(String(24), nullable=False, index=True)  # 用户名
     register_at = Column(DateTime, nullable=False, default=datetime.datetime.now, index=True)  # 注册时间，只要插入一次，所以用now()而不是now
     addresses = Column(String(4096), nullable=False, default="")  # 配送地址，用';'分隔
