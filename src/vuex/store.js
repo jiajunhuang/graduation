56dbd93f2f91c7166c85763f6779d656bf3b8ed8
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-04-09 18:30:53 +0800

    add store

---
+++ src/vuex/store.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex)
+Vue.config.debug = true
+
+// const debug = process.evv.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+
+})
