a56888878dd7456998912c19a74cb3d514abd6c1
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-04-17 21:10:31 +0800

    add modules

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import cart from './modules/cart'
 
 Vue.use(Vuex)
 Vue.config.debug = true
 
-// const debug = process.evv.NODE_ENV !== 'production'
-
 export default new Vuex.Store({
-
+  modules: {
+    cart
+  }
 })
