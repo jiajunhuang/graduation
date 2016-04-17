607244184ca16328cd0b91c7fc93f9b9e28aa6ab
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-04-17 21:12:46 +0800

    addToCart

--- src/vuex/modules/cart.js
+++ src/vuex/modules/cart.js
@@ -1,14 +1,19 @@
-// import {
-//   ADD_TO_CART
-// } from '../mutation-types'
-//
-// const state = {
-//   added: [],
-//   lastCheckout: null
-// }
-//
-// const mutations = {
-//   // [ADD_TO_CART](state, productId) {
-//   //
-//   // }
-// }
+import {
+  ADD_TO_CART
+} from '../mutation-types'
+
+const state = {
+  foods: []
+}
+
+const mutations = {
+  [ADD_TO_CART](state, food) {
+    state.foods.push(food)
+    console.log(state.foods)
+  }
+}
+
+export default {
+  state,
+  mutations
+}
