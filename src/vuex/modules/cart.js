2dc61eb75c8b679f4f52ef6548156612928962b1
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-04-19 21:39:20 +0800

    modify food.quantity

--- src/vuex/modules/cart.js
+++ src/vuex/modules/cart.js
@@ -1,15 +1,27 @@
 import {
-  ADD_TO_CART
+  ADD_TO_CART,
+  DELETE_ALL
 } from '../mutation-types'
 
 const state = {
-  foods: []
+  foods: [],
+  sum: 0
 }
 
 const mutations = {
   [ADD_TO_CART](state, food) {
-    state.foods.push(food)
-    console.log(state.foods)
+    const record = state.foods.find(p => p.fid === food.fid)
+    if (!record === true) {
+      food.quantity = 1
+      state.foods.push(food)
+    } else {
+      food.quantity = food.quantity + 1
+    }
+    state.sum = state.sum + 1
+  },
+  [DELETE_ALL](state) {
+    state.foods.length = 0
+    state.sum = 0
   }
 }
 
