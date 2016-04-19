9ea4c60e55804eaa2cbefd4dca619deef1a3d220
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-04-19 21:38:15 +0800

    add deleteAll

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -3,3 +3,7 @@ import * as types from './mutation-types'
 export const addToCart = ({ dispatch }, food) => {
   dispatch(types.ADD_TO_CART, food)
 }
+
+export const deleteAll = ({ dispatch }) => {
+  dispatch(types.DELETE_ALL)
+}
