c4022cee1e8e9e634967a37a32aa497829587f1c
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-05-02 22:04:47 +0800

    add excavator

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import cart from './modules/cart'
+import excavator from './modules/excavator'
 
 Vue.use(Vuex)
 Vue.config.debug = true
 
 export default new Vuex.Store({
   modules: {
-    cart
+    cart,
+    excavator
   }
 })
