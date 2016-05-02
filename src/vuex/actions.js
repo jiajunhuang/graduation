5bc39ddd8c479a85f2669d37a960607ba92150a0
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-05-02 22:03:47 +0800

    add

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -7,3 +7,31 @@ export const addToCart = ({ dispatch }, food) => {
 export const deleteAll = ({ dispatch }) => {
   dispatch(types.DELETE_ALL)
 }
+
+export const sortByDefault = ({ dispatch }) => {
+  dispatch(types.SORT_BY_DEFAULT)
+}
+
+export const sortBySalesCount = ({ dispatch }) => {
+  dispatch(types.SORT_BY_SALES_COUNT)
+}
+
+export const sortByGrade = ({ dispatch }) => {
+  dispatch(types.SORT_BY_GRADE)
+}
+
+export const sortByLowestMoney = ({ dispatch }) => {
+  dispatch(types.SORT_BY_LOWEST_MONEY)
+}
+
+export const filterByIsNewer = ({ dispatch }) => {
+  dispatch(types.FILTER_BY_IS_NEWER)
+}
+
+export const filterByFreeDeliver = ({ dispatch }) => {
+  dispatch(types.FILTER_BY_IS_FREE_DELIVER)
+}
+
+export const filterByIsInvoice = ({ dispatch }) => {
+  dispatch(types.FILTER_BY_IS_INVOICE)
+}
