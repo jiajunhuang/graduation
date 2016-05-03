81533b9261be75c39f5c996f36f35dd30ebe8d25
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-05-03 22:21:15 +0800

    add UPDATE_STATE

--- src/vuex/mutation-types.js
+++ src/vuex/mutation-types.js
@@ -1,5 +1,6 @@
 export const ADD_TO_CART = 'ADD_TO_CART'
 export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS'
+export const UPDATE_STATE = 'UPDATE_STATE'
 export const SORT_BY_DEFAULT = 'SORT_BY_DEFAULT'
 export const SORT_BY_SALES_COUNT = 'SORT_BY_SALES_COUNT'
 export const SORT_BY_GRADE = 'SORT_BY_GRADE'
