acecf99e17eaa265d9261158eb686386df16e2f5
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-05-03 22:23:44 +0800

    add sort function

--- src/vuex/modules/excavator.js
+++ src/vuex/modules/excavator.js
@@ -1,4 +1,5 @@
 import {
+  UPDATE_STATE,
   SORT_BY_DEFAULT,
   SORT_BY_SALES_COUNT,
   SORT_BY_GRADE,
@@ -9,18 +10,52 @@ import {
 } from '../mutation-types'
 
 const state = {
-  type: 'default'
+  shops: [],
+  initData: {}
+}
+
+const compare = (a,  b, key) => {
+  if ((a.key === undefined) || (b.key === undefined)) throw Error('compare:key undefined.')
+  if (a.key - b.key > 0) {
+    return -1
+  }
+  if (a.key - b.key < 0) {
+    return 1
+  }
+  return 0
 }
 
 const mutations = {
+  [UPDATE_STATE](state, data) {
+    console.log(data)
+    state.shops = data.shops
+    state.initData = Object.assign({}, data)
+  },
   [SORT_BY_DEFAULT](state) {
     console.log(state)
+    // state.shops = shops
   },
   [SORT_BY_SALES_COUNT](state) {
-    console.log(state)
+    state.shops.sort((a, b) => {
+      if (a.sales_count - b.sales_count > 0) {
+        return -1
+      }
+      if (a.sales_count - b.sales_count < 0) {
+        return 1
+      }
+      return 0
+    })
   },
   [SORT_BY_GRADE](state) {
-    console.log(state)
+    state.shops.sort((a, b) => {
+      if (a.sales_count - b.sales_count > 0) {
+        return -1
+      }
+      if (a.sales_count - b.sales_count < 0) {
+        return 1
+      }
+      return 0
+    })
   },
   [SORT_BY_LOWEST_MONEY](state) {
     console.log(state)
@@ -34,7 +69,6 @@ const mutations = {
   [FILTER_BY_IS_INVOICE](state) {
     console.log(state)
   }
-
 }
 
 export default {
