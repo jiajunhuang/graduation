2eb6dd1275e091289dde1ceefaf79bf80c8cde8e
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-06-05 17:06:44 +0800

    add

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,12 @@
+<template>
+  <register></register>
+</template>
+
+<script>
+import Register from '../components/Register'
+export default {
+  name: 'RegisterViwe',
+  components: [ Register ]
+}
+
+</script>
